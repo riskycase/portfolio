75238c4bfeb1a244e229fb503d23fa850d4901a2
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2024-08-13 16:02:19 +0530

    Added functionality to report links

--- src/components/redirect/redirectVisibleComponent.tsx
+++ src/components/redirect/redirectVisibleComponent.tsx
@@ -22,6 +22,18 @@ export default async function RedirectVisibleComponent({
       <LinkComponent as={NextLink} href={link.longLink} wordBreak="break-all">
         {link.longLink}
       </LinkComponent>
+      <Text>
+        In case this does not seem right, you can report the link below
+      </Text>
+      <LinkComponent
+        as={NextLink}
+        href={`${process.env.NEXT_PUBLIC_LINK_SHORTENER_HOME}/${link.shortCode}/report`}
+        wordBreak="break-all"
+      >
+        <Text>
+          {process.env.NEXT_PUBLIC_LINK_SHORTENER_HOME}/{link.shortCode}/report
+        </Text>
+      </LinkComponent>
     </Flex>
   );
 }
