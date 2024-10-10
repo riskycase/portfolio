24b9e957c99f1fb2dcff06b139fbdbe3a24e65e7
Hrishikesh Patil <hrishikeshpatil.754@gmail.com>
2024-10-10 11:23:09 +0530

    Add cloudflare image loader
    Signed-off-by: Hrishikesh Patil <hrishikeshpatil.754@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,12 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        loader: "custom",
+        loaderFile: "./imageLoader.js"
+    }
+};
 
 if (process.env.NODE_ENV === "development") {
     await setupDevPlatform();
